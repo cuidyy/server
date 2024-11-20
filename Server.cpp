06101a9891931ddda0c4e3893a01d367d3eaeba8
cuidy <cuidy@example.com>
2024-11-20 18:01:11 +0800

    class Server done

--- Server.cpp
+++ Server.cpp
@@ -1,11 +1,84 @@
 #include "Server.h"
 
-void Server::run()
+Server::Server()
 {
-    event();
+
 }
 
-void Server::event()
+Server::~Server()
+{
+    //释放监听器资源
+    evconnlistener_free(listener);
+
+    //释放base
+    event_base_free(base);
+}
+
+//运行
+void Server::run()
+{
+    init();
+}
+
+//服务器初始化
+void Server::init()
+{
+    //初始化事件基础架构
+    base = event_base_new();
+    if(base == nullptr)
+    {
+        cout << "base 创建失败" << endl;
+        exit(1);
+    }
+
+    //初始化服务器socket
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(8080);
+
+    //初始化监听器对象
+    listener = evconnlistener_new_bind(base, accept_cb, base, LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE, -1, (struct sockaddr*)&saddr, sizeof(saddr));
+    if(listener == nullptr)
+    {
+        cout << "listener 创建失败" << endl;
+        exit(1);
+    }
+
+    //开启事件主循环
+    event_base_dispatch(base);
+}
+
+void accept_cb(struct evconnlistener *listen, evutil_socket_t fd, 
+                    struct sockaddr * addr, int socklen, void *arg)
+{
+    char ip[32] = {0};
+    evutil_inet_ntop(AF_INET, addr, ip, sizeof(ip)-1);
+    printf("accept a client fd:%d ip:%s\n",fd,ip);
+
+    struct event_base *base = (struct event_base *)arg;    //把base传进来了
+
+    //构建bufferevent对象 对fd创建bufferevent事件
+    struct bufferevent *bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+
+    //设置读回调函数、异常处理函数
+    bufferevent_setcb(bev, read_cb, NULL, event_cb, NULL);
+    bufferevent_enable(bev,EV_READ | EV_PERSIST);   //注册事件
+}
+
+void read_cb(struct bufferevent *bev, void *arg)
 {
     
+}
+void event_cb(struct bufferevent *bev, short events, void *arg)
+{
+    if(events & BEV_EVENT_EOF)//与客户端断开连接
+        printf("断开连接\n");
+    else if (events & BEV_EVENT_ERROR)
+        printf("发送错误\n");
+    else if (events & BEV_EVENT_TIMEOUT)
+        printf("超时\n");
+
+    //释放bufferevent资源
+    bufferevent_free(bev);
 }
--- main.cpp
+++ main.cpp
@@ -1,66 +1,9 @@
 #include <iostream>
+#include "Server.h"
 using namespace std;
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h> 
-#define MAXLINE 4096
 
-int main(int argc, char** argv) {
-int listenfd, connfd;
-struct sockaddr_in serveraddr;
-char buff[MAXLINE];
-int n;
-
-if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
-exit(0);
-}
-
-memset(&serveraddr, 0, sizeof(serveraddr));
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-serveraddr.sin_port = htons(8080);
-
-if (bind(listenfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == -1) {
-printf("bind socket error: %s(errno: %d)\n", strerror(errno), errno);
-exit(0);
-}
-
-if (listen(listenfd, 10) == -1) {
-printf("listen socket error: %s(errno: %d)\n", strerror(errno), errno);
-exit(0);
-}
-
-printf("======waiting for client's request======\n");
-while (1) {
-if ((connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1) {
-printf("accept socket error: %s(errno: %d)\n", strerror(errno), errno);
-continue;
-}
-printf("====== OK ======\n");
-
-    while (1) {
-        
-        n = recv(connfd, buff, MAXLINE, 0);
-    
-        if (n == -1) {
-            printf("recv socket error: %s(errno: %d)\n", strerror(errno), errno);
-            break;
-        } else if (n == 0) {
-            // 客户端关闭了连接，退出内层循环
-            break;
-        }
-        buff[n] = '\0';
-        printf("recv msg from client: %s\n", buff);
-    }
-}
-printf("exit \n");
-close(connfd);
-close(listenfd);
+int main()
+{
+    Server s;
+    s.run();
 }
